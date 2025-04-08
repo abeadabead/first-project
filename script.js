e1dbe5e713d8286472a11b4df0317670c200ab8a
abdullahareflinkedin@gmail.com <abdullahareflinkedin@gmail.com>
2025-04-08 15:51:56 +0300

    fitst commit

---
+++ script.js
@@ -0,0 +1,56 @@
+// let title = ' welcome to our gay,';
+
+// function apartment ()
+// {
+// let kichen = 0 ;
+// let pedrome = 1;
+//  function room ()
+// {
+// let light = 0;
+// ++ light ;
+// kichen ++;
+// pedrome ++
+
+// console.log(light ,  kichen , pedrome);
+
+// }
+// return room;
+// }
+
+
+// {
+//  let a = '10.7';
+
+//  const b = true;
+
+
+ 
+
+// //console.log(parseFloat(a)/ parseFloat(b));
+
+
+// //console.log(a);
+// //console.log(b);
+// }
+
+
+const langs = ['arpic' , 'frince' , 'english'
+
+
+];
+
+console.log(langs);
+
+langs.push('name');
+langs.pop();
+
+
+
+
+
+
+
+
+
+
+
